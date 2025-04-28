fea608e8f5b947824f2415d8b1e5b60b3477a44a
Karim <k.nassar@lynxtrading.com>
2025-04-28 22:57:18 +0100

    Add test for GC

--- tests/test_add.py
+++ tests/test_add.py
@@ -3,7 +3,7 @@ import pytest
 from raypy import i64, f64, add
 
 
-class TestAddFloat:
+class TestAdd:
     @pytest.mark.parametrize(
         "x, y, expected",
         [
@@ -18,15 +18,6 @@ class TestAddFloat:
             (-5, 10, 5),
             (i64(10), 5, 15),
             (i64(20), i64(30), 50),
-        ],
-    )
-    def test_scalar_adding(self, x, y, expected):
-        result = add(x, y)
-        assert result == expected, f"Expected {expected}, got {result}"
-
-    @pytest.mark.parametrize(
-        "x, y, expected",
-        [
             ([1, 2, 3], 3, [4, 5, 6]),
             ([1, 2, 3], 3.1, [4.1, 5.1, 6.1]),
             (3.1, [1, 2, 3], [4.1, 5.1, 6.1]),
@@ -34,6 +25,6 @@ class TestAddFloat:
             ([1, 2, 3], [4.5, 5.5, 6.5], [5.5, 7.5, 9.5]),
         ],
     )
-    def test_list_adding(self, x, y, expected):
+    def test_add(self, x, y, expected):
         result = add(x, y)
         assert result == expected, f"Expected {expected}, got {result}"
