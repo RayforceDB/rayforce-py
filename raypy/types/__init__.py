3d8c83d558cc15f4393ff678240c05a88ded9d29
Karim <k.nassar@lynxtrading.com>
2025-05-03 22:52:10 +0100

    Implement time

--- raypy/types/__init__.py
+++ raypy/types/__init__.py
@@ -2,6 +2,7 @@ from .integer import i16, i32, i64, from_python_integer
 from .float import f64, from_python_float
 from .bool import b8, from_python_boolean
 from .date import Date, from_python_date
+from .time import Time, from_python_time
 
 __all__ = [
     "i16",
@@ -14,4 +15,6 @@ __all__ = [
     "from_python_boolean",
     "Date",
     "from_python_date",
+    "Time",
+    "from_python_time",
 ]
