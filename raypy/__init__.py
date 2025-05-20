cdee351977b3d49339eb638927aa6b8adefc4b13
Anton <singaraiona@gmail.com>
2025-05-20 20:15:33 +0200

    fix(init): os specific path

--- raypy/__init__.py
+++ raypy/__init__.py
@@ -3,9 +3,18 @@ Raypy - Python bindings for Rayforce library
 """
 
 import os
+import sys
 import ctypes
 
-lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "librayforce.dylib")
+# Get the correct library name based on the OS
+if sys.platform == "darwin":
+    lib_name = "librayforce.dylib"
+elif sys.platform == "win32":
+    lib_name = "rayforce.dll"
+else:  # Linux and other Unix-like
+    lib_name = "_rayforce.so"
+
+lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), lib_name)
 if os.path.exists(lib_path):
     try:
         ctypes.CDLL(lib_path, mode=ctypes.RTLD_GLOBAL)
