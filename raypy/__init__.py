63e5135cdf809610f5cf8f08f86ebcded3fcfa35
Karim <k.nassar@lynxtrading.com>
2025-05-28 10:56:59 +0100

    Remove useless imports

--- raypy/__init__.py
+++ raypy/__init__.py
@@ -1,17 +1,3 @@
 """
 Raypy - Python bindings for Rayforce library
 """
-
-from .event_loop import (
-    RayforceEventLoop,
-    RayforceEventLoopPolicy,
-    SelectorType,
-    PollEvents,
-)
-
-__all__ = [
-    "RayforceEventLoop",
-    "RayforceEventLoopPolicy",
-    "SelectorType",
-    "PollEvents",
-]
