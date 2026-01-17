405e3a0241e0ce1de27eaeb9336b82cf679015b9
Karim <k.nassar@lynxtrading.com>
2026-01-17 15:50:20 +0100

    release: 0.5.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name="rayforce_py",
-    version="0.5.3",
+    version="0.5.4",
     packages=find_packages(),
     package_data={
         "rayforce": ["*.so", "*.dylib", "*.pyi", "bin/rayforce"],
