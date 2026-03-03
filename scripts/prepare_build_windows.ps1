9da3c5df9f5e404d13dd52a04155db0010ecd7b1
Karim <k.nassar@lynxtrading.com>
2026-03-03 15:38:01 +0000

    10 attempt

--- scripts/prepare_build_windows.ps1
+++ scripts/prepare_build_windows.ps1
@@ -54,7 +54,7 @@ $pythonTarget = @"
 PY_OBJECTS = core/rayforce_c.o core/raypy_init_from_py.o core/raypy_init_from_buffer.o core/raypy_read_from_rf.o core/raypy_queries.o core/raypy_io.o core/raypy_binary.o core/raypy_dynlib.o core/raypy_eval.o core/raypy_iter.o core/raypy_serde.o
 PY_APP_OBJECTS = app/term.o
 python: CFLAGS = `$(RELEASE_CFLAGS) -DPY_SSIZE_T_CLEAN -I$PYTHON_INCLUDE -Wno-macro-redefined
-python: LDFLAGS = -fuse-ld=lld -L$PYTHON_LIB_DIR -lpython$PYTHON_PYVER
+python: LDFLAGS = -fuse-ld=lld -static -L$PYTHON_LIB_DIR -lpython$PYTHON_PYVER
 python: `$(CORE_OBJECTS) `$(PY_OBJECTS) `$(PY_APP_OBJECTS)
 	`$(CC) -shared -o _rayforce_c.pyd `$(CFLAGS) `$(CORE_OBJECTS) `$(PY_OBJECTS) `$(PY_APP_OBJECTS) `$(LIBS) `$(LDFLAGS)
 "@
@@ -80,7 +80,7 @@ Write-Host "Building Rayforce shared library..."
 $sharedWinTarget = @"
 
 shared-win: CFLAGS = `$(RELEASE_CFLAGS)
-shared-win: LDFLAGS = -fuse-ld=lld -Wl,--out-implib,rayforce.lib
+shared-win: LDFLAGS = -fuse-ld=lld -static -Wl,--out-implib,rayforce.lib
 shared-win: `$(CORE_OBJECTS) app/term.o
 	`$(CC) -shared -o `$(LIBNAME) `$(CFLAGS) `$(CORE_OBJECTS) app/term.o `$(LIBS) `$(LDFLAGS)
 "@
